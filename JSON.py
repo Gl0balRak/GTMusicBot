2a5320f96dad93f87ad3a12efb4287eea3193f15
Todrow <pinkol2005@gmail.com>
2021-12-27 13:18:06 +0300

    JSON added comments

--- JSON.py
+++ JSON.py
@@ -1,12 +1,12 @@
 import json
 
 
-def readJSON(idServer):
+def readJSON(idServer):  # Reading server playlist data from JSON
     with open('./playlists/playlists.json', 'r') as read_file:
         return json.load(read_file)[str(idServer)]
 
 
-def writeJSON(idServer, playlists):
+def writeJSON(idServer, playlists):  # Writing data from server playlists to JSON
     data = readJSON(idServer)
     data[str(idServer)] = playlists
     with open('./playlists/playlists.json', 'w') as write_file:
